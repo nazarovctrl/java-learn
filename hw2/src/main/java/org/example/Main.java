a7629c74db805261acba2404dbbe92d3d638bb87
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-04-19 23:40:14 +0500

    Input changed

--- hw2/src/main/java/org/example/Main.java
+++ hw2/src/main/java/org/example/Main.java
@@ -4,27 +4,15 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws DirectoryNotFound {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         File directory;
-        try {
-            directory = getDirectory();
-        } catch (DirectoryNotFound e) {
-            System.out.println(e.getMessage());
-            directory = getDirectory();
-        }
+        do {
+            System.out.println("Input directory path:");
+            String path = scanner.nextLine().trim();
+            directory = new File(path);
+        } while (!directory.isDirectory());
 
         FileScanner.startScan(directory);
-    }
-
-    public static File getDirectory() throws DirectoryNotFound {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input directory path:");
-        String path = scanner.nextLine().trim();
-
-        File directory = new File(path);
-        if (!directory.isDirectory()) {
-            throw new DirectoryNotFound(path);
-        }
-        return directory;
     }
 }
